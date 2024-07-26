9273794c02d4c0fed1e163c4c53b04ce7cfc7b57
Mahmoud Khaled <1902328@eng.asu.edu.eg>
2024-07-26 22:59:43 +0300

    bash

---
+++ ETL_toll_data.py
@@ -0,0 +1,70 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'dummy_name',
+    'start_date': datetime.now(),
+    'email': ['dummy_email@example.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -xzf /home/project/airflow/dags/finalassignment/tolldata.tgz',
+    dag=dag,
+)
+
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command='cut -d"," -f1,2,3,4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > \
+     /home/project/airflow/dags/finalassignment/csv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command='cut -f5,6,7 /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > \
+    /home/project/airflow/dags/finalassignment/tsv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command='cut -c59-67 /home/project/airflow/dags/finalassignment/payment-data.txt\
+     | tr " " "," > \
+     /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
+    dag=dag,
+)
+
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    bash_command='paste -d"," /home/project/airflow/dags/finalassignment/csv_data.csv \
+    /home/project/airflow/dags/finalassignment/tsv_data.csv \
+    /home/project/airflow/dags/finalassignment/fixed_width_data.csv > \
+    /home/project/airflow/dags/finalassignment/extracted_data.csv',
+    dag=dag,
+)
+
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command="awk -F',' '{OFS=\",\"; $4=toupper($4)}1' \
+    /home/project/airflow/dags/finalassignment/extracted_data.csv > \
+    /home/project/airflow/dags/finalassignment/transformed_data.csv",
+    dag=dag,
+)
+
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv \
+>> extract_data_from_fixed_width >> consolidate_data >> transform_data
+
+
